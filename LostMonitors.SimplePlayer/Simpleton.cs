e573bdb549fb153400311fc383babf27680b9153
Leo Chen <coel@users.noreply.github.com>
2016-01-11 01:46:16 +1100

    Implement basic movement

--- LostMonitors.SimplePlayer/Simpleton.cs
+++ LostMonitors.SimplePlayer/Simpleton.cs
@@ -1,13 +1,27 @@
 ﻿using System;
+using System.Linq;
 using LostMonitors.Core;
 
 namespace LostMonitors.SimplePlayer
 {
     public class Simpleton : IPlayer
     {
-        public void Play(BoardState currentState, Turn theirMove, Func<Turn, Destination, Card> draw)
+        public void Play(BoardState currentState, Turn theirMove, Func<Turn, Card> draw)
         {
-            throw new NotImplementedException();
+            var yourCards = currentState.YourCards.OrderBy(x => x.Value);
+            var myTurn = new Turn(yourCards.First()) { Discard = true };
+
+            foreach (var card in yourCards)
+            {
+                var yourExpedition = currentState.YourExpeditions[card.Destination];
+                if (!yourExpedition.Any() || card.Value > yourExpedition.Last().Value)
+                {
+                    myTurn = new Turn(card);
+                    break;
+                }
+            }
+
+            draw(myTurn);
         }
 
         public string GetFriendlyName()
