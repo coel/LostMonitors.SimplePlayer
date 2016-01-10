9b968910d8a3c6f6fb270c6f9735593ff224fcac
Leo Chen <coel@users.noreply.github.com>
2016-01-10 21:22:27 +1100

    Initial commit

---
+++ LostMonitors.SimplePlayer/Simpleton.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using LostMonitors.Core;
+
+namespace LostMonitors.SimplePlayer
+{
+    public class Simpleton : IPlayer
+    {
+        public void Play(BoardState currentState, Turn theirMove, Func<Turn, Destination, Card> draw)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
