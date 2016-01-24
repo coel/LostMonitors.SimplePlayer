8053943c1354dd60bb2db0f2e9d1382043ead246
Leo Chen <coel@users.noreply.github.com>
2016-01-24 14:23:14 +1100

    Just play the lowest card so we get discards earlier

--- LostMonitors.SimplePlayer/Simpleton.cs
+++ LostMonitors.SimplePlayer/Simpleton.cs
@@ -6,19 +6,24 @@ namespace LostMonitors.SimplePlayer
 {
     public class Simpleton : IPlayer
     {
+        private const int MaxExpeditions = 3;
+
         public void Play(BoardState currentState, Turn theirMove, Func<Turn, Card> draw)
         {
             var yourCards = currentState.YourCards.OrderBy(x => x.Value);
-            var myTurn = new Turn(yourCards.First()) { Discard = true };
+            var myTurn = new Turn(yourCards.First());
 
-            foreach (var card in yourCards)
+            var yourExpedition = currentState.YourExpeditions[myTurn.Card.Destination].OrderBy(x => x.Value);
+
+            var expeditionsRunning = currentState.YourExpeditions.Count(x => x.Value.Any());
+            if (!yourExpedition.Any() && expeditionsRunning >= MaxExpeditions)
             {
-                var yourExpedition = currentState.YourExpeditions[card.Destination].OrderBy(x => x.Value);
-                if (!yourExpedition.Any() || card.Value >= yourExpedition.Last().Value)
-                {
-                    myTurn = new Turn(card);
-                    break;
-                }
+                myTurn.Discard = true;
+            }
+
+            if (yourExpedition.Any() && myTurn.Card.Value < yourExpedition.Last().Value)
+            {
+                myTurn.Discard = true;
             }
 
             draw(myTurn);
