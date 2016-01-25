5975dc6daf4d2648b2b519244e92b1c537909b69
Leo Chen <coel@users.noreply.github.com>
2016-01-26 10:38:08 +1100

    Add a second player which only discards

---
+++ LostMonitors.SimplePlayer/Dummy.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+using LostMonitors.Core;
+
+namespace LostMonitors.SimplePlayer
+{
+    public class Dummy : IPlayer
+    {
+        public void Play(BoardState currentState, Turn theirMove, Func<Turn, Card> draw)
+        {
+            var yourCards = currentState.YourCards.OrderByDescending(x => x.Value);
+
+            var myTurn = new Turn(yourCards.First()) {Discard = true};
+
+            draw(myTurn);
+        }
+
+        public string GetFriendlyName()
+        {
+            return "Dummy McDumdum";
+        }
+    }
+}
